caaac74b78f47847ed946d5ce18042757b5bd99b
Lim-ss <w757001674@gmail.com>
2024-01-31 19:10:15 +0800

    配好了glew环境

--- OpenGL/OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/OpenGL/src/Application.cpp
@@ -1,5 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
+#include <iostream>
 int main(void)
 {
     GLFWwindow* window;
@@ -7,6 +8,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -18,6 +20,11 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK)
+        std::cout<<"glewinit error"<<std::endl;
+    else
+        std::cout << "glewinit succeed" << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
